1be6c70e0a253ff2bd0bb51c1a8b6206ddf51b5c
maheini35 <maheini35@me.com>
2022-01-16 00:06:42 +0100

    first implementation of a Card

--- lib/Card.dart
+++ lib/Card.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Card extends StatefulWidget {
-  const Card({Key? key}) : super(key: key);
+  Card(String? text, {VoidCallback? onChanged, Key? key}) : super(key: key);
+  String? text;
 
   @override
   _CardState createState() => _CardState();
 }
 
 class _CardState extends State<Card> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    controller.text = widget.text?? '';
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(5),
+      child: TextField(
+        controller: controller,
+      ),
+    );
   }
 }
