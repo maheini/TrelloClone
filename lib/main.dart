970265f76fdfc9bd71180b3c793371832a30cb95
maheini35 <maheini35@me.com>
2022-01-20 15:19:27 +0100

    changed add button for a new List

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
+    if(_todoLists.isEmpty){
+      _addNewListButton(_todoLists);
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('TrelloClone  ${_todoLists.length}'),
@@ -36,18 +39,52 @@ class _MainScreenState extends State<MainScreen> {
           children: _todoLists,
         )
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => setState(() {
-          _addTodoList('name $counter', _todoLists);
-          counter++;
-        })
-      ),
     );
   }
   TextEditingController controller = TextEditingController();
 
+  void _addNewListButton(List<Widget> todoLists){
+    Widget addNewListButton = Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
+          width: 350,
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(4),
+            border: Border.all(color: Colors.grey[400]!, width: 2),
+          ),
+          child: Container(
+            height: 40,
+            width: double.infinity,
+            margin: const EdgeInsets.all(5),
+            child: TextButton(
+              onPressed: () => setState(() {
+                _addTodoList('', todoLists);
+              }),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black.withOpacity(0.05)),
+              ),
+              child: Row(
+                children: [
+                  const Icon(Icons.add),
+                  Container(width: 10,),
+                  const Text('add list'),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    todoLists.add(addNewListButton);
+  }
+
   void _addTodoList(String name, List<Widget> todoLists) {
-    todoLists.add(TodoList(key: UniqueKey(), name: name,));
+    todoLists.last = TodoList(key: UniqueKey(), name: name,);
+
+    _addNewListButton(todoLists);
   }
 }
